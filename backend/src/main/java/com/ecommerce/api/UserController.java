2fe79eba20addc5ddb6a7a8ee03fb0f48dce1368
김승범 <tmdwkd2@naver.com>
2020-09-09 16:45:23 +0900

    # S03P22B101-13 Add findpw function BE

--- backend/src/main/java/com/ecommerce/api/UserController.java
+++ backend/src/main/java/com/ecommerce/api/UserController.java
@@ -8,10 +8,16 @@ import com.ecommerce.domain.exception.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -20,7 +26,10 @@ public class UserController {
     public static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private IUserService userService;
-
+    
+    @Autowired
+    private JavaMailSender mailSender;
+    
     @Autowired
     public UserController(IUserService userService) {
         Assert.notNull(userService, "userService 개체가 반드시 필요!");
@@ -72,5 +81,45 @@ public class UserController {
     public void delete(@PathVariable int id) {
         userService.delete(id);
     }
-
+    
+    //비밀번호 찾기
+    @RequestMapping(value = "/users/findpw", method = RequestMethod.POST)
+    public User get(@RequestBody User user) {
+    	User users = userService.get(user.getEmail());
+        if (users == null || !users.getName().equals(user.getName())) {
+        	logger.error("NOT FOUND : ", user.getName());
+            throw new NotFoundException(user.getName() + " 회원 정보를 찾을 수 없습니다.");
+        }
+        return users;
+    }
+    
+    //비밀번호 찾기
+    @RequestMapping(value = "/users/sendemail", method = RequestMethod.POST)
+    public User sendEmail(@RequestBody User user) {
+    	// 임시 비밀번호 생성 저장 변수
+		String userPwd = "";
+		for (int i = 0; i < 12; i++) {
+			userPwd += (char) ((Math.random() * 26) + 97);
+		}
+		userPwd += (int)(Math.random() * 9)+1;
+		
+		// 임시비밀번호로 업데이트
+		user.setPassword(userPwd);
+		userService.update(user);
+		
+		// 메일보내기
+		try {
+			MimeMessage msg = mailSender.createMimeMessage();
+			MimeMessageHelper messageHelper = new MimeMessageHelper(msg, true, "UTF-8");
+			messageHelper.setFrom("tmdwkd2.gmail.com"); // 보내는사람의 주소(없으면 동작x)
+			messageHelper.setTo(user.getEmail()); // 받는사람의 주소
+			messageHelper.setSubject(user.getName() + "님의 비밀번호 찾기 메일입니다."); // 제목
+			messageHelper.setText("임시 패스워드는  " + userPwd + " 입니다."); // 내용
+			msg.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse(user.getEmail()));
+			mailSender.send(msg);
+		} catch (MessagingException e) {
+			
+		}
+        return user;
+    }
 }
