3b8911c74bc9df062139fee7df1227871b6035da
권민지 <ming___jee@naver.com>
2020-09-22 14:04:28 +0900

    #S03P23B101-45 Add DealList function FE (not finished yet)

--- frontend/src/views/deal/DealList.vue
+++ frontend/src/views/deal/DealList.vue
@@ -2,14 +2,40 @@
   <div>
     <h-nav></h-nav>
     <div class="container" style="text-align: center;">
-      <h3 style="margin-top: 100px;">티켓거래 페이지</h3>
-      <router-link
+      <h3>티켓거래 페이지</h3>
+      <div>
+          <table class="table table-striped" style="width: 100%; margin: auto;">
+              <thead class="thead-dark">
+                  <tr>
+                      <th>거래번호</th>
+                      <th>분류</th>
+                      <th>공연명</th>
+                      <th>날짜</th>
+                      <th>회차(시간대)</th>
+                      <th>좌석 등급</th>
+                      <th>가격</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr v-for="(deal, index) in deal_list" :key="index" :deal_list="deal_list">
+                      <td>{{index+1}}</td>
+                      <td>category</td>
+                      <td>title</td>
+                      <td>{{ deal.date }}</td>
+                      <td>{{ deal.time }}</td>
+                      <td>{{ deal.grade }}석</td>
+                      <td>{{ deal.price }}</td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+      <!-- <router-link
         :to="{ name: 'dealregister' }"
         class="btn btn-primary"
         style="color: white; background-color: #FF4155"
       >
           거래등록
-      </router-link>
+      </router-link> -->
     </div>
   </div>
 
@@ -18,14 +44,38 @@
 <script>
 import HNav from "@/components/common/HNav";
 import axios from "axios";
+import { API_BASE_URL } from "@/config";
 
 export default {
   components: {
     HNav
   },
+  data: () => {
+      return {
+          deal_list: []
+      }
+  },
+  created() {
+      axios
+        .get(API_BASE_URL + '/api/deal')
+        .then((res) => {
+            this.deal_list = res.data;
+            console.log(this.deal_list)
+        })
+        .catch((err) => {
+            console.log("created axios get method error!")
+        })
+  },
+  methods() {
+
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+ h3 {
+     margin-top: 100px;
+     font-weight: bold;
+     margin-bottom: 10px;
+ }
 </style>
